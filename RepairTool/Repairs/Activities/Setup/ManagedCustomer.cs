2145376a8cb6b9a216bfbd8ee866000a03ce70e5
Joshua Langer <josh.r.langer@gmail.com>
2022-06-21 17:09:39 -0500

    Added additional options for Initial Setup

---
+++ RepairTool/Repairs/Activities/Setup/ManagedCustomer.cs
@@ -0,0 +1,65 @@
+﻿using RepairTool.Core;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepairTool.Repairs.Activities.Setup
+{
+    public static class ManagedCustomer
+    {
+        public static void BeginSetup()
+        {
+            InstallVSA();
+            SystemPrep.BeginPrep();
+        }
+
+        private static void InstallVSA()
+        {
+            var runFile = EnvironmentVars.INITSETUP + "ManagedCustomers\\VSASetup.exe";
+            using (StreamWriter w = File.AppendText(EnvironmentVars.LOGFILE))
+            {
+                Logger.LogInfo("Installing RMM Tool...", w);
+            }
+            // Prepare the process to run
+            ProcessStartInfo start = new ProcessStartInfo();
+            start.UseShellExecute = false;
+            start.RedirectStandardOutput = true;
+            start.RedirectStandardError = true;
+
+            // Enter the executable to run, including the complete path
+            start.FileName = runFile;
+            // Do you want to show a console window?
+            start.WindowStyle = ProcessWindowStyle.Hidden;
+            start.CreateNoWindow = true;
+            int exitCode;
+
+
+            // Run the external process & wait for it to finish
+            using (Process proc = Process.Start(start))
+            {
+                proc.WaitForExit();
+                System.Threading.Thread.Sleep(30000);
+                var output = proc.StandardOutput.ReadToEnd();
+                using (StreamWriter w = File.AppendText(EnvironmentVars.LOGFILE))
+                {
+                    Logger.LogInfo(output, w);
+                }
+                // Retrieve the app's exit code
+                exitCode = proc.ExitCode;
+            }
+
+            using (StreamWriter w = File.AppendText(EnvironmentVars.LOGFILE))
+            {
+                Logger.LogInfo("VSA Installed...", w);
+            }
+            using (StreamWriter w = File.AppendText(EnvironmentVars.LOGFILE))
+            {
+                Logger.LogInfo("Remaining sections of deployment to come from RMM Dashboard...", w);
+            }
+        }
+    }
+}
---
+++ RepairTool/Repairs/Activities/Setup/SetupMenu.cs
@@ -0,0 +1,58 @@
+﻿using RepairTool.Core;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepairTool.Repairs.Activities.Setup
+{
+    public static class SetupMenu
+    {
+        public static void Menu()
+        {
+            Console.Clear();
+            Console.Title = "Setup and Provision Tool - " + EnvironmentVars.APPVERSION;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.WriteLine(EnvironmentVars.COMPANYNAME + "            Setup and Provision Tool " + EnvironmentVars.APPVERSION);
+            Console.WriteLine("Please enter a selection for your phase of repair");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("           1. Managed Services Customer");
+            Console.WriteLine("           2. Bench Customer");
+            Console.WriteLine("           0. Exit");
+            var choice = ReadInt("Enter your phase: ");
+            RepairLauncher(choice);
+        }
+
+        private static void RepairLauncher(int choice)
+        {
+            switch (choice)
+            {
+                case 1:
+                    ManagedCustomer.BeginSetup();
+                    break;
+                case 2:
+                    SystemPrep.BeginPrep();
+                    break;
+                case 0:
+                    using (StreamWriter w = File.AppendText(EnvironmentVars.SYSTEMLOGS))
+                    {
+                        Logger.LogInfo("Application is closing cleanly..." + EnvironmentVars.NORMALEXITCODE, w);
+                    }
+                    Environment.Exit(EnvironmentVars.NORMALEXITCODE);
+                    break;
+            }
+        }
+
+        private static int ReadInt(string text)
+        {
+            Console.WriteLine(text);
+            var line = Console.ReadLine();
+            return int.Parse(line);
+        }
+    }    
+}
+
