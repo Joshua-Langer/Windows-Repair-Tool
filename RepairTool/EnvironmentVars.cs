50da7b179b39b5dff06545e4d327b8055147dbae
Joshua Langer <josh.r.langer@gmail.com>
2022-06-20 22:16:56 -0500

    Attempted fix for Client/Server communication, no dice.

--- RepairTool/EnvironmentVars.cs
+++ RepairTool/EnvironmentVars.cs
@@ -47,7 +47,7 @@ namespace RepairTool
 
         // Additional strings used throughout the program - shown as statics for file paths as the Current Directory may change
         public static string ROOTDIR = "\\" + IPADDR;
-        public static string BINDIR = "C:\\wrtbin";
+        public static string BINDIR = Directory.GetCurrentDirectory() + "\\wrtbin";
         public static string LOGDIR = BINDIR +"\\logs\\";
         public static string RAWLOGDIR = BINDIR + "";
         public static string RESDIR = BINDIR + "\\resources\\";
