b51ce563bb816e30025c3957f25caaefab5130f9
Joshua Langer <josh.r.langer@gmail.com>
2022-06-25 11:31:23 -0500

    Started refactoring for build 204.

---
+++ RepairTool/Core/ProcessRunner.cs
@@ -0,0 +1,157 @@
+using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace RepairTool.Core
+{
+    public static class ProcessRunner
+    {
+        private const string CONSOLETITLE = "Windows Repair Tool - ";
+        
+        /// <summary>
+        /// Task runner to run a task without output logging. Overloads.
+        /// </summary>
+        /// <param name="repairType"></param>
+        /// <param name="taskName"></param>
+        /// <param name="runFile"></param>
+        /// <param name="arguments"></param>
+        public static void TaskRunner(string repairType, string taskName, string runFile, string arguments)
+        {
+            Console.Title = CONSOLETITLE + repairType + " - " + taskName + " " + EnvironmentVars.APPVERSION;
+            using (StreamWriter w = File.AppendText(EnvironmentVars.LOGFILE))
+            {
+                Logger.LogInfo(repairType + " " + taskName + " is running", w);
+            }
+
+            ProcessStartInfo start = new ProcessStartInfo();
+            start.UseShellExecute = false;
+            start.RedirectStandardOutput = true;
+            start.RedirectStandardError = true;
+            start.Arguments = arguments;
+            start.FileName = runFile;
+            start.WindowStyle = EnvironmentVars.processWindowHide;
+            start.CreateNoWindow = EnvironmentVars.noConsoleWindow;
+
+            using (Process process = Process.Start(start))
+            {
+                process.WaitForExit();
+            }
+            using (StreamWriter w = File.AppendText(EnvironmentVars.LOGFILE))
+            {
+                Logger.LogInfo(taskName + " has completed...", w);
+            }
+        }
+
+        /// <summary>
+        /// Task runner to run a task with output logging but no error logging. Overloads.
+        /// </summary>
+        /// <param name="repairType"></param>
+        /// <param name="taskName"></param>
+        /// <param name="runFile"></param>
+        /// <param name="arguments"></param>
+        /// <param name="exitCode"></param>
+        public static void TaskRunner(string repairType, string taskName, string runFile, string arguments,
+            string exitCode)
+        {
+            Console.Title = CONSOLETITLE + repairType + " - " + taskName + " " + EnvironmentVars.APPVERSION;
+            using (StreamWriter w = File.AppendText(EnvironmentVars.LOGFILE))
+            {
+                Logger.LogInfo(repairType + " " + taskName + " is running", w);
+            }
+
+            ProcessStartInfo start = new ProcessStartInfo();
+            start.UseShellExecute = false;
+            start.RedirectStandardOutput = true;
+            start.RedirectStandardError = true;
+            start.Arguments = arguments;
+            start.FileName = runFile;
+            start.WindowStyle = EnvironmentVars.processWindowHide;
+            start.CreateNoWindow = EnvironmentVars.noConsoleWindow;
+
+            using (Process process = Process.Start(start))
+            {
+                process.WaitForExit();
+                System.Threading.Thread.Sleep(15000);
+                var output = process.StandardOutput.ReadToEnd();
+                using (StreamWriter w = File.AppendText(EnvironmentVars.LOGFILE))
+                {
+                    Logger.LogInfo(output, w);
+                }
+            }
+            using (StreamWriter w = File.AppendText(EnvironmentVars.LOGFILE))
+            {
+                Logger.LogInfo(taskName + " has completed...", w);
+            }
+            using (StreamWriter w = File.AppendText(EnvironmentVars.LOGFILE))
+            {
+                Logger.LogInfo("Completed Code: " + exitCode, w);
+            }
+        }
+
+        /// <summary>
+        /// Task runner to run a task with output logging and error logging.
+        /// Uses errorCodeToAvoid to check against errors in a called process.
+        /// </summary>
+        /// <param name="repairType"></param>
+        /// <param name="taskName"></param>
+        /// <param name="runFile"></param>
+        /// <param name="arguments"></param>
+        /// <param name="exitCode"></param>
+        /// <param name="errorCodeToAvoid"></param>
+        public static void TaskRunner(string repairType, string taskName, string runFile, string arguments,
+            string exitCode, int errorCodeToAvoid)
+        {
+            EnvironmentVars.ErrorsDetected = false; // Set to false in case an error was detected in a previous method run
+            Console.Title = CONSOLETITLE + repairType + " - " + taskName + " " + EnvironmentVars.APPVERSION;
+            using (StreamWriter w = File.AppendText(EnvironmentVars.LOGFILE))
+            {
+                Logger.LogInfo(repairType + " " + taskName + " is running", w);
+            }
+
+            ProcessStartInfo start = new ProcessStartInfo();
+            start.UseShellExecute = false;
+            start.RedirectStandardOutput = true;
+            start.RedirectStandardError = true;
+            start.Arguments = arguments;
+            start.FileName = runFile;
+            start.WindowStyle = EnvironmentVars.processWindowHide;
+            start.CreateNoWindow = EnvironmentVars.noConsoleWindow;
+
+            var error = "";
+            
+            using (Process process = Process.Start(start))
+            {
+                process.WaitForExit();
+                System.Threading.Thread.Sleep(15000);
+                var output = process.StandardOutput.ReadToEnd();
+                error = process.StandardError.ReadToEnd();
+                using (StreamWriter w = File.AppendText(EnvironmentVars.LOGFILE))
+                {
+                    Logger.LogInfo(output, w);
+                }
+                using (StreamWriter w = File.AppendText(EnvironmentVars.LOGFILE))
+                {
+                    Logger.LogInfo(error, w);
+                }
+            }
+
+            if (error != errorCodeToAvoid.ToString())
+            {
+                EnvironmentVars.ErrorsDetected = true; // Set to true in case we need to run additional methods.
+                using (StreamWriter w = File.AppendText(EnvironmentVars.LOGFILE))
+                {
+                    Logger.LogError(taskName + " has completed with errors...", w);
+                }
+            }
+            
+            using (StreamWriter w = File.AppendText(EnvironmentVars.LOGFILE))
+            {
+                Logger.LogInfo(taskName + " has completed without errors...", w);
+            }
+            using (StreamWriter w = File.AppendText(EnvironmentVars.LOGFILE))
+            {
+                Logger.LogInfo("Completed Code: " + exitCode, w);
+            }
+        }
+    }
+}
